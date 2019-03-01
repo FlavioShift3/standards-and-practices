c971ff2335f8096be3c2b9906bb9afa343234e1e
mwallert <mwallert@shift3tech.com>
2019-02-28 16:10:21 -0800

    chore(final updates)

--- aws/terraform/elastic-beanstalk-cloudfront/main.tf
+++ aws/terraform/elastic-beanstalk-cloudfront/main.tf
@@ -38,18 +38,18 @@ resource "aws_route53_record" "cert_validation" {
   ttl     = 60
 }
 
-resource "aws_route53_record" "cert_validation_east" {
-  name    = "${aws_acm_certificate.cert_cloudfront_east.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.cert_cloudfront_east.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.zone.id}"
-  records = ["${aws_acm_certificate.cert_cloudfront_east.domain_validation_options.0.resource_record_value}"]
-  ttl     = 60
-}
-
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = "${aws_acm_certificate.cert.arn}"
   validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
 }
+
+# resource "aws_route53_record" "cert_validation_east" {
+#   name    = "${aws_acm_certificate.cert_cloudfront_east.domain_validation_options.0.resource_record_name}"
+#   type    = "${aws_acm_certificate.cert_cloudfront_east.domain_validation_options.0.resource_record_type}"
+#   zone_id = "${data.aws_route53_zone.zone.id}"
+#   records = ["${aws_acm_certificate.cert_cloudfront_east.domain_validation_options.0.resource_record_value}"]
+#   ttl     = 60
+# }
 
 resource "aws_s3_bucket" "web_bucket" {
   bucket = "${var.web_domain_name}"
